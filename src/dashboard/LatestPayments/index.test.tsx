2816a9ecb3fcf3c79e6df9d03e61f5adb28231da
dtafesse <dtafesse@uwm.edu>
2021-03-09 21:32:58 -0600

    updating the tests for the LatestPayments component

--- src/dashboard/LatestPayments/index.test.tsx
+++ src/dashboard/LatestPayments/index.test.tsx
@@ -14,12 +14,17 @@ jest.mock('./api.ts');
 
 const getPaymentMocked = createMockedFunction(api.getPayment);
 
-beforeAll(() => {
+// for some reason, with the way i restructured the first test (move the time second by second, and validate every second),
+// i need to use 'jest.useFakeTimers()' without the 'modern' param,
+// and also need to use a beforeEach instead of a beforeAll - not sure
+// why beforeAll does not work...
+beforeEach(() => {
   // we're using fake timers because we don't want to
   // wait a full second for this test to run.
   // this will make our tests more reliable
-  jest.useFakeTimers('modern');
+  jest.useFakeTimers();
 });
+
 afterAll(() => {
   jest.useRealTimers();
 });
@@ -42,20 +47,34 @@ test('updates latest payments every second 3 times', async () => {
   expect(screen.getByRole('progressbar')).toBeInTheDocument();
   expect(getPaymentMocked).toHaveBeenCalledTimes(0);
 
-  // advance the timers by three seconds.
+  // advance the timer by 1 second.
   // when the interval fires and the cb functions run, interally it is setting some react state.
   // this state change is happening outside of the react call stack, so react does not know when to
   // make this change happen, run any effects on re-render, and finally get the dom in a stable state.
   // since this line is the line that is making this state change happen, we will wrap it in an 'act'
   act(() => {
-    jest.advanceTimersByTime(3000);
+    jest.advanceTimersByTime(1000);
   });
 
   // state changes is triggered asynchronously and therefore can happen after act finishes.
   // so will use a query that will handle async scenerios.
   // according to the docs, "findBy queries work when you expect an element to appear but the change to the DOM might not happen immediately."
   expect(await screen.findByText(firstPayment.date)).toBeInTheDocument();
+  expect(getPaymentMocked).toHaveBeenCalledTimes(1);
+
+  // advance the timer by 1 second
+  act(() => {
+    jest.advanceTimersByTime(1000);
+  });
+
   expect(await screen.findByText(secondPayment.date)).toBeInTheDocument();
+  expect(getPaymentMocked).toHaveBeenCalledTimes(2);
+
+  // advance the timer by 1 second
+  act(() => {
+    jest.advanceTimersByTime(1000);
+  });
+
   expect(await screen.findByText(thirdPayment.date)).toBeInTheDocument();
   expect(getPaymentMocked).toHaveBeenCalledTimes(3);
 });
@@ -94,6 +113,13 @@ test('filtering payments', async () => {
     jest.advanceTimersByTime(2000);
   });
 
+  // both payments should be visible
+  await waitFor(() => {
+    expect(screen.queryByText(firstPayment.sender.name)).toBeInTheDocument();
+    expect(screen.queryByText(secondPayment.sender.name)).toBeInTheDocument();
+  });
+
+  // grab the search field
   const searchField = screen.getByRole('textbox');
   fireEvent.change(searchField, { target: { value: searchQuery } });
 
